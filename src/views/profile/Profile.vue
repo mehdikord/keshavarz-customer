5eda780d2ba241193767c175bd4ebb8543a9be3f
Mehdi Kord <mehdi.kord171@gmail.com>
2025-04-04 17:04:04 +0330

    upgrade

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -271,7 +271,7 @@ export default {
           </div>
           <div class="row q-mt-lg">
             <div class="col-4 ">
-              <q-icon name="fusera-duotone fa-solid fa-house-building" color="green-8" size="18px"></q-icon>
+              <q-icon name="fa-duotone fa-solid fa-house-building" color="green-8" size="18px"></q-icon>
               <strong v-if="user.province" class="q-ml-xs text-grey-9 font-13"> : {{user.province.name}}</strong>
             </div>
             <div class="col-4">
